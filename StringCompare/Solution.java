0523323487b3b7c12b8c994a8942fce434a4c58e
kw1234 <karan@appformix.com>
2018-11-19 13:18:39 -0800

    Reorg directory structure, add StringCompare

---
+++ StringCompare/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+    public Solution() {
+
+    }
+
+    public boolean isMatch(String input, String pattern) {
+	
+	int inIndex = 0;
+	int patIndex = 0;
+	
+    }
+
+    public static void main(String[] args) {
+	Solution sol = new Solution();
+	Str in1 = "aab";
+	String pat1 = "a*b";
+
+	String pat2 = "c*a*b";
+
+	String pat3 = ".*";
+    }
+
+}
