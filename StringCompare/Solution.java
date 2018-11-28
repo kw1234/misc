a0e48b7a4a5409aa42958408e20ba8aecc08579c
kw1234 <karan@appformix.com>
2018-11-27 18:29:50 -0800

    asa

--- StringCompare/Solution.java
+++ StringCompare/Solution.java
@@ -8,6 +8,35 @@ public class Solution {
 	
 	int inIndex = 0;
 	int patIndex = 0;
+
+	char prevInCh = ' ';
+	char prevPatCh = ' ';
+
+	boolean matchBool = true;
+
+	while (inIndex < input.length() && patIndex < pattern.length()) {
+	    char inCh = input.charAt(inIndex);
+	    char patCh = pattern.charAt(patIndex);
+
+	    if (patCh == '.') {
+		inIndex++;
+		patIndex++;
+	    } else if (patCh == '*') {
+		if (prevPatCh == prevInCh && inCh == prevPatCh) {
+		    
+		}
+	    } else {
+		if (inCh != patCh) {
+		    matchBool = false;
+		}
+		inIndex++;
+		patIndex++;
+	    }
+
+	    prevInCh = inCh;
+	    prevPatCh = patCh;
+	    
+	}
 	
     }
 
